d4f9c3a81ec385fe9cbc35dea61548fad8971b7c
sroshc <shirzaysroshsafa@gmail.com>
2025-07-31 16:31:53 -0400

    added main menu scene

--- loophole/MAIN/main.gd
+++ loophole/MAIN/main.gd
@@ -3,4 +3,4 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	get_tree().change_scene_to_file("res://ROOMS/Outside.tscn") # Replace with menu later
+	pass
