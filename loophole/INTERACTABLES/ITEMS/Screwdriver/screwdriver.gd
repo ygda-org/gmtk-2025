903a9ef18928d7626279064c71a031710408b8a6
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2025-07-31 17:57:13 -0400

    2 keys and screwdriver

---
+++ loophole/INTERACTABLES/ITEMS/Screwdriver/screwdriver.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@export var itemResource: BaseItemResource # for inventory and use functions
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func interact():
+	for i in range(len(GameState.player_inventory)):
+		if GameState.player_inventory[i] == null:
+			GameState.player_inventory[i] = itemResource # BaseItemResources go into the inventory array
+			break
+
+	
