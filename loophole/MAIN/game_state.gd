c0bff68354571914d6c9bea1bbfd82b69d88211a
sroshc <shirzaysroshsafa@gmail.com>
2025-07-31 17:22:53 -0400

    added saving system

--- loophole/MAIN/game_state.gd
+++ loophole/MAIN/game_state.gd
@@ -2,6 +2,18 @@ extends Node
 
 var player_inventory = [BaseItemResource] # what data type should this hold?
 
+
+const SAVE_PATH: String = "user://savedata.json"
+const START_SAVE_DATA : Dictionary[String, int] = {
+	"disguise_path": 0,
+	"vent_path": 0,
+	"keys_path": 0,
+	"sewer_path": 0,
+	"inspector_path": 0,
+}
+
+var loaded_save_data: Dictionary = {}
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	reset_player_inventory()
@@ -13,3 +25,38 @@ func _process(delta):
 	
 func reset_player_inventory():
 	GameState.player_inventory = [null, null, null, null, null, null]
+
+
+func create_new_save_file():
+	var json_save_data: String = JSON.stringify(START_SAVE_DATA)
+	
+	var file_access := FileAccess.open(SAVE_PATH, FileAccess.WRITE)
+	if not file_access:
+		print("Error occured while trying to write save data:", FileAccess.get_open_error())
+		return
+
+	file_access.store_line(json_save_data)
+	file_access.close()
+	print("Created save file at: " + file_access.get_path_absolute())
+	
+	loaded_save_data = START_SAVE_DATA
+	
+func load_save_file() -> int:
+	if not FileAccess.file_exists(SAVE_PATH):
+		print("Couldn't find the save data!")
+		return FAILED
+	
+	var file_access := FileAccess.open(SAVE_PATH, FileAccess.READ)
+	var json_string := file_access.get_line()
+	
+	var json := JSON.new()
+	var error = json.parse(json_string)
+	if error:
+		print("Couldn't parse the JSON save data: " + json.get_error_message())
+		print("Save data: " + json_string)
+		return FAILED
+	
+	loaded_save_data = json.data
+	print("Loaded save data: " + str(loaded_save_data))
+	return OK
+	
--- loophole/MAIN/main.gd
+++ loophole/MAIN/main.gd
@@ -3,18 +3,12 @@ extends Node2D
 const STARTING_SCENE = preload("res://ROOMS/Outside.tscn")
 
 func _on_load_pressed() -> void:
-	if load_save_file() == OK:
+	if GameState.load_save_file() == OK:
 		SceneSwitcher.goto_scene(STARTING_SCENE, "")
 	else:
 		print("Unabled to load save file! Maybe it doesn't exist?")
 
 func _on_new_game_pressed() -> void:
-	create_new_save_file()
+	GameState.create_new_save_file()
 	
 	SceneSwitcher.goto_scene(STARTING_SCENE, "")
-	
-func load_save_file() -> int:
-	return OK
-	
-func create_new_save_file():
-	pass
