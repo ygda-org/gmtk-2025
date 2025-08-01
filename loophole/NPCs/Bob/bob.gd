3686e0a2c9bb3183f73e5ab2fa73f0e4f67a9bbf
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2025-07-31 20:05:30 -0400

    Create bob

---
+++ loophole/NPCs/Bob/bob.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func interact():
+	DialogueManager.show_dialogue_balloon(load("res://NPCs/Bob/test.dialogue"))
