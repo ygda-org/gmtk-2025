9148fe15f93db70c1afa5f05773ec7a7b19a58b1
sroshc <shirzaysroshsafa@gmail.com>
2025-08-01 13:24:30 -0400

    added start of intro cut scene

--- loophole/ROOMS/SCENE_MANAGEMENT/cutscene_manager.gd
+++ loophole/ROOMS/SCENE_MANAGEMENT/cutscene_manager.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
-@export var cutscene_path: String
-var dialogue_resource = load(cutscene_path)
+@export var dialogue_resource: DialogueResource
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
