b898fae994f0b4b543522d5fc18822a3176d840f
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2025-08-01 15:00:28 -0400

    Create locked door
    Add collision and interaction and fill in the resource of the item used to unlock it

---
+++ loophole/INTERACTABLES/locked_door.gd
@@ -0,0 +1,25 @@
+extends StaticBody2D
+
+@export var unlockResource: BaseItemResource
+@export var consumesUnlock: bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func interact():
+	for i in range(len(GameState.player_inventory)):
+		if GameState.player_inventory[i] == null or unlockResource.itemName == null:
+			continue
+		if unlockResource.itemName == GameState.player_inventory[i].itemName:
+			if consumesUnlock:
+				GameState.player_inventory[i] == null
+			unlock()
+
+func unlock():
+	queue_free() # prolly might wanna do this differently later
