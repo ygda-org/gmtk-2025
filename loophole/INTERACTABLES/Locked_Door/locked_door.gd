dd8bbdffc548aa80698abf2a57bf1c90cd6bfb41
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2025-08-02 16:11:31 -0400

    Made the door open!

--- loophole/INTERACTABLES/Locked_Door/locked_door.gd
+++ loophole/INTERACTABLES/Locked_Door/locked_door.gd
@@ -4,14 +4,13 @@ extends StaticBody2D
 @export var consumesUnlock: bool = false
 @export var dialogue: DialogueResource = load("res://INTERACTABLES/Locked_Door/Locked_Door.dialogue")
 
+var animations: AnimatedSprite2D = null
+
+var unlocked = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 func interact():
 	DialogueManager.show_dialogue_balloon(dialogue)
@@ -24,7 +23,21 @@ func attempt():
 		if unlockResource.itemName == GameState.player_inventory[i].itemName:
 			if consumesUnlock:
 				GameState.player_inventory[i] == null
-			unlock()
+			unlocked = true
+
+func _process(delta):
+	if unlocked and GameState.can_player_move:
+		unlocked = false
+		unlock()
 
 func unlock():
-	queue_free() # prolly might wanna do this differently later
+	$CollisionShape2D.disabled = true
+	$Interactable/CollisionShape2D.disabled = true
+	animations = get_node("Anim")
+	if animations == null:
+		return
+	animations.play("open")
+	animations.animation_finished.connect(_animation_finished)
+
+func _animation_finished():
+	animations.play("opened")
