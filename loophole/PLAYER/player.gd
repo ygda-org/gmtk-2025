81287e1dd24deb795f382525a14ec5f85fa19a50
sroshc <shirzaysroshsafa@gmail.com>
2025-08-03 08:58:11 -0400

    added player animations

--- loophole/PLAYER/player.gd
+++ loophole/PLAYER/player.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 
 const SPEED = 100
 
+@onready var anim: AnimatedSprite2D = $Anim
+var current_direction: Vector2 = Vector2(0, 0)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -20,7 +23,30 @@ func _physics_process(delta):
 		if interactables.size() >= 1 and interactables[0].is_in_group("interactable"):
 			interactables[0].interact()
 	
-	velocity = Input.get_vector("left", "right", "up", "down") * SPEED
-	if velocity.y > 0:
-		$Anim.play("walk-down")
+	var input_vector: Vector2 = Input.get_vector("left", "right", "up", "down")
+	
+	current_direction = input_vector if input_vector != Vector2.ZERO else current_direction 
+	velocity =  input_vector * SPEED
+	
+	
+	if velocity == Vector2.ZERO:
+		if current_direction.y > 0:
+			anim.play("idle_down")
+		elif current_direction.y < 0:
+			anim.play("idle_up")
+		elif current_direction.x > 0:
+			anim.play("idle_right")
+		elif current_direction.x < 0:
+			anim.play("idle_left")
+	else:
+		if velocity.y > 0:
+			anim.play("walk_down")
+		elif velocity.y < 0:
+			anim.play("walk_up")
+		elif velocity.x > 0:
+			anim.play("walk_right")
+		elif velocity.x < 0:
+			anim.play("walk_left")
+	
+	
 	move_and_slide()
